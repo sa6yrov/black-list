f8082b1b8ace070a170debeabef8a0d8b6d1612a
sa6yrov <sabyrov.aidin@gmail.com>
2021-06-11 23:16:48 +0600

    first commit

---
+++ src/main/java/kg/weare/blacklist/blacklist/BlackListApplication.java
@@ -0,0 +1,13 @@
+package kg.weare.blacklist.blacklist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlackListApplication.class, args);
+	}
+
+}
